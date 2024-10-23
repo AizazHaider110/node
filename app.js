6d8992d0c8793e46c3ea60520ababf6e989556c2
Aizaz Haider <aizaz265@gmail.com>
2024-10-23 15:36:44 +0500

    used async await

--- app.js
+++ app.js
@@ -20,20 +20,31 @@ app.set('views', path.join(__dirname, 'views'));
 
 // MongoDB connection (modify your dbURI as needed)
 const dbURI = 'mongodb+srv://aizaz265:aizaz265@node.y5ou5.mongodb.net/?retryWrites=true&w=majority&appName=node';
-mongoose.connect(dbURI)
-  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-  .catch(err => console.log(err));
+const startServer = async() => {
+  try {
+     await mongoose.connect(dbURI);
+     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  }  catch(err){
+    console.log(err);
+  }
+};
 
+startServer();
 
 
 // Routes
 // List all blogs
-app.get('/', (req, res) => {
-  Blog.find()
-    .then(blogs => {
-      res.render('index', { title: 'Home', blogs });
-    })
-    .catch(err => console.error(err));
+app.get('/', async (req, res) => {
+  try {
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    res.render('index', { title: 'Home', blogs });
+  } catch (err) {
+    console.error(err);
+    res.status(500).render('error', { 
+      title: 'Error', 
+      error: 'Failed to fetch blogs' 
+    });
+  }
 });
 
 app.get('/blogs/create', (req, res) => {
@@ -43,31 +54,32 @@ app.get('/blogs/create', (req, res) => {
   });
 });
 
-app.post('/blogs', (req, res) => {
-  const { title, snippet, body } = req.body;
-  
-  if (!title || !snippet || !body) {
-    return res.render('create', { 
-      title: 'Create a new blog', 
-      error: 'All fields are required' 
+app.post('/blogs', async (req, res) => {
+  try {
+    const { title, snippet, body } = req.body;
+    
+    if (!title || !snippet || !body) {
+      return res.render('create', {
+        title: 'Create a new blog',
+        error: 'All fields are required'
+      });
+    }
+
+    const newBlog = new Blog({
+      title,
+      snippet,
+      body
+    });
+
+    await newBlog.save();
+    res.redirect('/');
+  } catch (err) {
+    console.error('Error creating blog:', err);
+    res.render('create', {
+      title: 'Create a new blog',
+      error: 'Failed to create blog'
     });
   }
-
-  const newBlog = new Blog({
-    title,
-    snippet,
-    body
-  });
-
-  newBlog.save()
-    .then(() => res.redirect('/'))
-    .catch(err => {
-      console.error('Error creating blog:', err);
-      res.render('create', { 
-        title: 'Create a new blog', 
-        error: 'Failed to create blog' 
-      });
-    });
 });
 
 
@@ -87,47 +99,72 @@ app.get('/blogs/:id', (req, res) => {
     });
 });
 
-
-app.post('/blogs/:id/edit', (req, res) => {
-  const id = req.params.id;
-  const { title, snippet, body } = req.body;
-
-  if (!title || !snippet || !body) {
-    return res.render('edit', {
-      title: 'Edit Blog',
-      blog: { _id: id, title, snippet, body },
-      error: 'All fields are required'
+app.get('/blogs/:id/edit', async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) {
+      return res.status(404).render('404', { title: '404' });
+    }
+    res.render('edit', { 
+      title: 'Edit Blog', 
+      blog,
+      error: null 
     });
+  } catch (err) {
+    console.error('Error fetching blog:', err);
+    res.status(404).render('404', { title: '404' });
   }
+});
+app.post('/blogs/:id/edit', async (req, res) => {
+  try {
+    const { title, snippet, body } = req.body;
+    const id = req.params.id;
 
-  Blog.findByIdAndUpdate(id, { title, snippet, body }, { new: true })
-    .then(updatedBlog => res.redirect(`/blogs/${updatedBlog._id}`))
-    .catch(err => {
-      console.error('Error updating blog:', err);
-      res.render('edit', {
+    if (!title || !snippet || !body) {
+      return res.render('edit', {
         title: 'Edit Blog',
         blog: { _id: id, title, snippet, body },
-        error: 'Failed to update blog'
+        error: 'All fields are required'
       });
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      id, 
+      { title, snippet, body }, 
+      { new: true }
+    );
+
+    if (!updatedBlog) {
+      return res.status(404).render('404', { title: '404' });
+    }
+
+    res.redirect(`/blogs/${updatedBlog._id}`);
+  } catch (err) {
+    console.error('Error updating blog:', err);
+    res.render('edit', {
+      title: 'Edit Blog',
+      blog: { _id: req.params.id, ...req.body },
+      error: 'Failed to update blog'
     });
+  }
 });
 
-
-// Delete blog
-app.post('/blogs/:id/delete', (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then(() => res.redirect('/'))
-    .catch(err => {
-      console.error('Error deleting blog:', err);
-      res.status(500).render('error', { title: 'Error', error: 'Failed to delete blog' });
+app.post('/blogs/:id/delete', async (req, res) => {
+  try {
+    const deletedBlog = await Blog.findByIdAndDelete(req.params.id);
+    if (!deletedBlog) {
+      return res.status(404).render('404', { title: '404' });
+    }
+    res.redirect('/');
+  } catch (err) {
+    console.error('Error deleting blog:', err);
+    res.status(500).render('error', { 
+      title: 'Error', 
+      error: 'Failed to delete blog' 
     });
+  }
 });
 
-
-// 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
-
